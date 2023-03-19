4efaca41e91b4bf8961fc646035c7b0f59596679
flizzyflo <vip3r1@gmx.de>
2023-03-19 09:28:25 +0100

    implemented new constants

--- src/Settings/settings.py
+++
@@ -1,8 +0,0 @@
-RESOLUTION = HEIGHT, WIDTH = 500, 500
-
-BLOCKSIZE: int = 10
-FRAMERATE: int = 60
-WALL_COLOR: str = "white"
-START_GOAL_COLOR: str = "blue"
-EMPTY_COLOR: str = "white"
-BACKGROUND_COLOR: str = "black"
---
+++ src/settings/settings.py
@@ -0,0 +1,18 @@
+RESOLUTION = HEIGHT, WIDTH = 1000, 1000
+
+BLOCKSIZE: int = 10
+FRAMERATE: int = 60
+TITLE: str = "Search Algos Visualized"
+
+WALL_COLOR: str = "white"
+START_COLOR: str = "blue"
+GOAL_COLOR: str = "red"
+EMPTY_COLOR: str = "white"
+VISITED_COLOR: str = "lightblue"
+BACKGROUND_COLOR: str = "black"
+
+WALL_BLOCK: int = 1
+EMPTY_BLOCK: int = 0
+START_BLOCK: int = 2
+END_BLOCK: int = 3
+VISITED_BLOCK: int = 4
