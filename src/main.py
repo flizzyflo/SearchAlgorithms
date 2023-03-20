03f8c63ab2d6c7cb1008c274533cfbd1d85faabc
flizzyflo <vip3r1@gmx.de>
2023-03-20 13:43:24 +0100

    reduced main file, put logic into other files

--- src/main.py
+++ src/main.py
@@ -1,44 +1,9 @@
-import sys
+from src.settings.settings import TITLE
+from src.user_interface.control_interface import ControlInterface
 
-from src.map_structure.map_structure import MapStructure
-from src.search_algorithms.depth_first_search import Dfs
-from src.user_interface.application_window import ApplicationWindow
-from src.search_algorithms.breadth_first_search import Bfs
-
-
-def initialize_program(desired_algorithm: str) -> ApplicationWindow:
-    m = MapStructure(randomized_walls=False)
-
-    if desired_algorithm == "b":
-        search_algo = Bfs(map_structure=m)
-
-    elif desired_algorithm == "d":
-        search_algo = Dfs(map_structure=m)
-
-    w = ApplicationWindow(block_map=m,
-                          search_algorithm=search_algo)
-
-    return w
 
 if __name__ == '__main__':
 
-    a = initialize_program("d")
-
-    while True:
-
-        if a.search_is_over():
-            b = input("Again? (y/n) ")
-
-            if b == "y":
-                print("Select your algorithm: ")
-                desired_algorithm = input("Bfs: b, Dfs: d ")
-                while desired_algorithm not in ["b", "d"]:
-                    print("Please enter a valid input.")
-                    desired_algorithm = input("Bfs: b, Dfs: d ")
-                a = initialize_program(desired_algorithm)
-
-            elif b == "n":
-                sys.exit()
-
-        else:
-            a.run()
+    r = ControlInterface()
+    r.title(string=TITLE)
+    r.mainloop()
