c1e08e4cd411f66b14422b6c1171269f62752555
flizzyflo <vip3r1@gmx.de>
2023-03-20 13:40:22 +0100

    refactored code, made more modular

--- src/search_algorithms/algorithms_abstract_base_class.py
+++ src/search_algorithms/algorithms_abstract_base_class.py
@@ -1,12 +1,12 @@
 from abc import ABC, abstractmethod
 
 from src.map_structure.map_structure import MapStructure
-from src.settings.settings import WIDTH, HEIGHT
+from src.settings.settings import BLOCKSIZE, WALL_BLOCK
 
 
-class Algorithms(ABC):
+class Algorithm(ABC):
 
-    def __init__(self, map_structure) -> None:
+    def __init__(self, map_structure: MapStructure) -> None:
         self.is_visited: set[tuple[int, int]] = set()
         self.no_way_found: bool = False
         self.goal_found: bool = False
@@ -14,6 +14,8 @@ class Algorithms(ABC):
         self.start_coordinates: tuple[int, int] = (0, 0)
         self.goal_coordinates: tuple[int, int] = (0, 0)
         self.map_structure: MapStructure = map_structure
+        self.height = self.map_structure.get_heigth()
+        self.width = self.map_structure.get_width()
 
     @abstractmethod
     def perform_search(self) -> None:
@@ -31,31 +33,31 @@ class Algorithms(ABC):
     def initialize_start_coordinates(self, start_coordinates: tuple[int, int]) -> None:
         ...
 
-    def is_wall(self, block_coordinates: tuple[int, int]) -> bool:
+    def is_no_wall(self, block_coordinates: tuple[int, int]) -> bool:
         """
         Checks whether a coordinate passed in is a wall or not and returns either true or false
         """
 
         try:
-            return self.map_structure.final_map[block_coordinates] == 1
+            return self.map_structure.final_map[block_coordinates] != WALL_BLOCK
+
         except KeyError as ke:
-            pass
+            False
 
     def is_valid_coordinate(self, block_coordinates: tuple[int, int]) -> bool:
         """
         Wrapper method. Returns true if coordinates are no wall and are in bounds.
         """
 
-        return self.is_wall(block_coordinates=block_coordinates) is False and self.is_in_bounds(
-            block_coordinates=block_coordinates)
+        return (self.is_no_wall(block_coordinates=block_coordinates)) and self.is_in_bounds(block_coordinates=block_coordinates)
 
     def is_in_bounds(self, block_coordinates: tuple[int, int]) -> bool:
 
         """
-        Checks whether the coordinates are within the painted screen or not and return either true or false.
+        Checks whether the coordinates are within or are border of the painted screen
+        or not and return either true or false.
         """
-
-        return (0, 0) < block_coordinates < (WIDTH, HEIGHT)
+        return (0 <= block_coordinates[0] <= self.width - BLOCKSIZE) and (0 <= block_coordinates[1] <= self.height - BLOCKSIZE)
 
     def no_way_exists(self) -> bool:
         return self.no_way_found
