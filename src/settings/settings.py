47ac1bf7cd39888a6e2cbc8e1d6fb1aa6d61493d
flizzyflo <vip3r1@gmx.de>
2023-03-21 08:55:37 +0100

    changed text

--- src/settings/settings.py
+++ src/settings/settings.py
@@ -18,12 +18,13 @@ END_BLOCK: int = 3
 VISITED_BLOCK: int = 4
 
 INFORMATION_FRAME_TEXT="""User-Manual:
-\n1. Select the screen size or stick with default values.
-\n2. Select the  search-algorithm
-\n3. Select randomized walls or not
-\n4. Initialize the program. When changing things before starting, you need to re-initialize.
-\n5. Click 'Start' button. New window appears containing the program itself.
-\n ------- You started the program, all information below are relevant for the search-algorithm window -------
-\n6. Right mouse button: Set both, start (blue) and destination(red). Both are required to start.
-\n7. Left mouse button: Add additional or delete existing walls.
-\n8. Click any button on your keyboard to start the search algorithm."""
+\n1. Select the screen size or keep default values.
+\n2. Select search-algorithm
+\n3. Disable or enable randomized walls
+\n4. Initialize the program. Reinitialize when something is changed before start.
+\n5. Click 'Start'. New window appears containing the program itself.
+\n\n ------- Controls -------
+\n6. Right mouse button: Set both, start (blue) and destination (red). Both are required to start. Clicking the same
+block again removes the block.
+\n7. Left mouse button: Add additional wall. Clicking an existing wall will delete it.
+\n8. Click any button on your keyboard to perform the search"""
