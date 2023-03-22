09bf74654d9c19ec53b42feb23ac06bbb81b0cff
flizzyflo <vip3r1@gmx.de>
2023-03-22 21:52:11 +0100

    minor refactoring

--- src/search_algorithms/depth_first_search.py
+++ src/search_algorithms/depth_first_search.py
@@ -1,16 +1,13 @@
 from src.map_structure.map_structure import MapStructure
-from src.search_algorithms.algorithms_abstract_base_class import Algorithm
+from src.search_algorithms.search_algorithm_abstract_base_class import SearchAlgorithm
 from src.settings.settings import BLOCKSIZE, VISITED_BLOCK
 
 
-class Dfs(Algorithm):
+class Dfs(SearchAlgorithm):
 
     def __init__(self, map_structure: MapStructure) -> None:
         super().__init__(map_structure=map_structure)
         self.stack = list()
-
-    def initialize_goal_coordinates(self, goal_coordinates: tuple[int, int]):
-        self.goal_coordinates = goal_coordinates
 
     def initialize_start_coordinates(self, start_coordinates: tuple[int, int]):
         self.start_coordinates = start_coordinates
@@ -24,34 +21,40 @@ class Dfs(Algorithm):
             return
 
         # get next block to visit
-        cur_x, cur_y = self.stack.pop()
-        self.current_coordinates = (cur_x, cur_y)
+        current_block_x, current_block_y = self.stack.pop()
+        self.current_coordinates = (current_block_x, current_block_y)
 
-        # block was already visited, will be skipped
-        if self.current_coordinates in self.is_visited:
+        # check block was already visited, will be skipped
+        if self.already_visited_block(block_coordinates=self.current_coordinates):
             return
 
         # goal was found.
-        if self.current_coordinates == self.goal_coordinates:
-            self.goal_found = True
+        if self.is_destination(block_coordinates=self.current_coordinates):
+            self.destination_detected = True
             return
 
         # case way can exist and goal not detected. put neighbours to stack
         # blocksize is the step-length to take to get to the neighbour
-        if self.is_valid_coordinate(block_coordinates=(cur_x, cur_y - BLOCKSIZE)):
-            self.stack.append((cur_x, cur_y - BLOCKSIZE))
-        if self.is_valid_coordinate(block_coordinates=(cur_x - BLOCKSIZE, cur_y)):
-            self.stack.append((cur_x - BLOCKSIZE, cur_y))
-        if self.is_valid_coordinate(block_coordinates=(cur_x, cur_y + BLOCKSIZE)):
-            self.stack.append((cur_x, cur_y + BLOCKSIZE))
-        if self.is_valid_coordinate(block_coordinates=(cur_x + BLOCKSIZE, cur_y)):
-            self.stack.append((cur_x + BLOCKSIZE, cur_y))
 
-        self.is_visited.add(self.current_coordinates)
+        if self.is_valid_coordinate(block_coordinates=(current_block_x, current_block_y - BLOCKSIZE)):
+            self.stack.append((current_block_x, current_block_y - BLOCKSIZE))
+
+        if self.is_valid_coordinate(block_coordinates=(current_block_x - BLOCKSIZE, current_block_y)):
+            self.stack.append((current_block_x - BLOCKSIZE, current_block_y))
+
+        if self.is_valid_coordinate(block_coordinates=(current_block_x, current_block_y + BLOCKSIZE)):
+            self.stack.append((current_block_x, current_block_y + BLOCKSIZE))
+
+        if self.is_valid_coordinate(block_coordinates=(current_block_x + BLOCKSIZE, current_block_y)):
+            self.stack.append((current_block_x + BLOCKSIZE, current_block_y))
+
+        self.visited_this_block(block_coordinates=self.current_coordinates)
 
         # change values within the map structure for colorizing.
         # if current position is start position, then it should not be colorized
         if self.current_coordinates == self.start_coordinates:
             pass
+
         else:
+            # change map value to 'visited' to change color
             self.map_structure.final_map[self.current_coordinates] = VISITED_BLOCK
