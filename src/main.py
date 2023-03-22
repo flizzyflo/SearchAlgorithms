4d42c5cb6db829674733a700023c88558216f66f
flizzyflo <vip3r1@gmx.de>
2023-03-22 21:53:41 +0100

    refactoring

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 from src.settings.settings import TITLE
 from src.user_interface.control_interface import ControlInterface
 
-
 if __name__ == '__main__':
 
     root = ControlInterface()
     root.title(string=TITLE)
     root.mainloop()
+
