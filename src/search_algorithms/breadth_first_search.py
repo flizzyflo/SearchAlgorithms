47155c5ab08ed78968340ed2dff708dff9d6e456
flizzyflo <vip3r1@gmx.de>
2023-03-22 21:51:45 +0100

    removed method to base class, refactored

--- src/search_algorithms/breadth_first_search.py
+++ src/search_algorithms/breadth_first_search.py
@@ -1,10 +1,10 @@
 from src.map_structure.map_structure import MapStructure
 from src.settings.settings import BLOCKSIZE, VISITED_BLOCK
-from src.search_algorithms.algorithms_abstract_base_class import Algorithm
+from src.search_algorithms.search_algorithm_abstract_base_class import SearchAlgorithm
 from queue import Queue
 
 
-class Bfs(Algorithm):
+class Bfs(SearchAlgorithm):
 
     def __init__(self, map_structure: MapStructure) -> None:
         super().__init__(map_structure=map_structure)
@@ -14,9 +14,6 @@ class Bfs(Algorithm):
         self.start_coordinates = start_coordinates
         self.block_queue.put(self.start_coordinates)
 
-    def initialize_goal_coordinates(self, goal_coordinates: tuple[int, int]) -> None:
-        self.goal_coordinates = goal_coordinates
-
     def perform_search(self) -> None:
 
         # goal was not found
@@ -24,34 +21,43 @@ class Bfs(Algorithm):
             self.no_way_found = True
 
         # get next block to visit
-        cur_x, cur_y = self.block_queue.get()
-        self.current_coordinates = (cur_x, cur_y)
+        current_block_x, current_block_y = self.block_queue.get()
+        self.current_coordinates = (current_block_x, current_block_y)
 
-        if self.current_coordinates in self.is_visited:
+        if self.already_visited_block(block_coordinates=self.current_coordinates ):
             return
 
         # case: goal reached
-        if self.goal_detected(block_coordinates=self.current_coordinates):
-            self.goal_found = True
+        if self.is_destination(block_coordinates=self.current_coordinates):
+            self.destination_detected = True
             return
 
         # current coordinate is not visible / existing
-        if self.is_valid_coordinate(block_coordinates=(cur_x, cur_y)) is False:
+        if self.is_valid_coordinate(block_coordinates=(current_block_x, current_block_y)) is False:
             return
 
         # check any adjacent coordinate. blocksize is the step-length to take to get to the neighbour
-        if self.is_valid_coordinate(block_coordinates=(cur_x + BLOCKSIZE, cur_y)):
-            self.block_queue.put((cur_x + BLOCKSIZE, cur_y))
-        if self.is_valid_coordinate(block_coordinates=(cur_x - BLOCKSIZE, cur_y)):
-            self.block_queue.put((cur_x - BLOCKSIZE, cur_y))
-        if self.is_valid_coordinate(block_coordinates=(cur_x, cur_y - BLOCKSIZE)):
-            self.block_queue.put((cur_x, cur_y - BLOCKSIZE))
-        if self.is_valid_coordinate(block_coordinates=(cur_x, cur_y + BLOCKSIZE)):
-            self.block_queue.put((cur_x, cur_y + BLOCKSIZE))
+        if self.is_valid_coordinate(block_coordinates=(current_block_x + BLOCKSIZE, current_block_y)):
+            neighbour = (current_block_x + BLOCKSIZE, current_block_y)
+            self.block_queue.put(neighbour)
 
-        self.is_visited.add(self.current_coordinates)
+        if self.is_valid_coordinate(block_coordinates=(current_block_x - BLOCKSIZE, current_block_y)):
+            neighbour = (current_block_x - BLOCKSIZE, current_block_y)
+            self.block_queue.put(neighbour)
+
+        if self.is_valid_coordinate(block_coordinates=(current_block_x, current_block_y - BLOCKSIZE)):
+            neighbour = (current_block_x, current_block_y - BLOCKSIZE)
+            self.block_queue.put(neighbour)
+
+        if self.is_valid_coordinate(block_coordinates=(current_block_x, current_block_y + BLOCKSIZE)):
+            neighbour = (current_block_x, current_block_y + BLOCKSIZE)
+            self.block_queue.put(neighbour)
+
+
+        self.visited_this_block(block_coordinates=self.current_coordinates)
 
         if self.current_coordinates == self.start_coordinates:
             pass
         else:
+            # change map value to 'visited' to change color
             self.map_structure.final_map[self.current_coordinates] = VISITED_BLOCK
