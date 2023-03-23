72cfbcf23ff436923775b70e7a47e26446a0ffa4
flizzyflo <vip3r1@gmx.de>
2023-03-23 19:26:59 +0100

    refactored text

--- src/settings/settings.py
+++ src/settings/settings.py
@@ -1,6 +1,6 @@
-RESOLUTION = WIDTH, HEIGHT = 400, 400
+RESOLUTION = WIDTH, HEIGHT = 800, 800
 
-BLOCKSIZE: int = 5
+BLOCKSIZE: int = 8
 FRAMERATE: int = 60
 TITLE: str = "Search Algos Visualized"
 
@@ -10,7 +10,7 @@ GOAL_COLOR: str = "red"
 EMPTY_COLOR: str = "white"
 VISITED_COLOR: str = "lightblue"
 BACKGROUND_COLOR: str = "black"
-SHORTEST_PATH_COLOR: str = "orange"
+SHORTEST_PATH_COLOR: str = "lightgreen"
 
 WALL_BLOCK: int = 1
 EMPTY_BLOCK: int = 0
@@ -20,13 +20,13 @@ VISITED_BLOCK: int = 4
 SHORTEST_PATH: int = 5
 
 INFORMATION_FRAME_TEXT = """User-Manual:
-\n1. Select the screen size or keep default values.
-\n2. Select search-algorithm
-\n3. Disable or enable randomized walls
-\n4. Initialize the program. Reinitialize when something is changed before start.
-\n5. Click 'Start'. New window appears containing the program itself.
+\n1. Set screen size
+\n2. Select algorithm
+\n3. Disable/enable randomized walls
+\n4. Initialize the program. Reinitialize when something is changed before start
+\n5. 'Start' - New window appears containing the program itself.
 \n\n ------- Controls -------
-\n6. Right mouse button: Set both, start (blue) and destination (red). Both are required to start. Clicking the same
-block again removes the block.
-\n7. Left mouse button: Add additional wall. Clicking an existing wall will delete it.
-\n8. Click any button on your keyboard to perform the search"""
+\n6. Right mouse button: Set both start (blue) and destination (red). Both are required to start. Clicking the same
+block again removes the block
+\n7. Left mouse button: Add additional wall. Clicking an existing wall will remove it.
+\n8. Press enter to start"""
