50b13cff7613c2e7ce93485b1be85b0eac0af2d6
flizzyflo <vip3r1@gmx.de>
2023-03-27 18:44:51 -0400

    renaming of variables

--- src/search_algorithms/path_finding_algorithms/node.py
+++ src/search_algorithms/path_finding_algorithms/node.py
@@ -9,16 +9,17 @@ class Node:
                  coordinates: tuple[int, int],
                  predecessor_coordinates: tuple[int, int] = None,
                  successor_coordinates: tuple[int, int] = None,
-                 g_value: int = 0,
-                 h_value: int = 0,
-                 f_value: int = 0):
+                 real_distance: int = 0,
+                 estimated_distance: int = 0,
+                 total_distance: int = 0):
 
         self.coordinates: tuple[int, int] = coordinates
+        self.x, self.y = self.coordinates
         self.predecessor_coordinates: tuple[int, int] = predecessor_coordinates
         self.successor_coordinates: tuple[int, int] = successor_coordinates
-        self.g_value: int = g_value  # distance cost of node
-        self.h_value: int = h_value  # heuristic estimated cost of node
-        self.f_value: int = f_value  # total cost of node (g + h)
+        self.real_distance: int = real_distance  # distance cost of node
+        self.estimated_distance: int = estimated_distance  # heuristic estimated cost of node
+        self.total_distance: int = total_distance  # total cost of node (g + h)
         self.enqueued: bool = False
         Node.all_nodes[self.coordinates] = self
 
@@ -38,37 +39,37 @@ class Node:
         self.successor_coordinates = successor_coordinates
 
     def get_real_distance_travel_costs(self) -> int:
-        return self.g_value
+        return self.real_distance
 
     def set_real_distance_travel_costs_to(self, g_value: int) -> None:
-        self.g_value = g_value
+        self.real_distance = g_value
 
     def get_heuristic_distance_travel_costs(self) -> int:
-        return self.h_value
+        return self.estimated_distance
 
     def set_heuristic_travel_costs_to(self, h_value: int) -> None:
-        self.h_value = h_value
+        self.estimated_distance = h_value
 
     def set_total_travel_costs_to(self, f_value: int) -> None:
-        self.f_value = f_value
+        self.total_distance = f_value
 
     def __repr__(self):
         return f"{self.coordinates} -> {self.predecessor_coordinates}"
 
     def __eq__(self, other: Self) -> bool:
-        return self.f_value == other.f_value
+        return self.total_distance == other.total_distance
 
     def __lt__(self, other: Self) -> bool:
-        return self.f_value < other.f_value
+        return self.total_distance < other.total_distance
 
     def __le__(self, other: Self) -> bool:
-        return self.f_value <= other.f_value
+        return self.total_distance <= other.total_distance
 
     def __gt__(self, other: Self) -> bool:
-        return self.f_value > other.f_value
+        return self.total_distance > other.total_distance
 
     def __ge__(self, other: Self) -> bool:
-        return self.f_value >= other.f_value
+        return self.total_distance >= other.total_distance
 
     def __iter__(self):
         return iter(self.get_coordinates())
