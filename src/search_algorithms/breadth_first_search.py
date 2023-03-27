4f71c5f385ca3e680780ecc2fee9709d512133af
flizzyflo <vip3r1@gmx.de>
2023-03-27 18:42:56 -0400

    small refactoring

--- src/search_algorithms/breadth_first_search.py
+++ src/search_algorithms/breadth_first_search.py
@@ -1,9 +1,9 @@
-import time
+
+from queue import Queue
 
 from src.map_structure.map_structure import MapStructure
-from src.settings.settings import BlockColors
 from src.search_algorithms.search_algorithm_abstract_base_class import SearchAlgorithm
-from queue import Queue
+from src.settings.settings import BlockColors
 
 
 class Bfs(SearchAlgorithm):
@@ -19,14 +19,14 @@ class Bfs(SearchAlgorithm):
     def get_next_block(self) -> tuple[int, int]:
         return self.block_queue.get()
 
-    def perform_search(self, next_block: tuple[int, int]) -> None:
+    def perform_search(self, current_block_to_investigate: tuple[int, int]) -> None:
 
         # goal was not found
         if self.block_queue.empty():
             self.no_way_found = True
 
         # get next block to visit
-        current_block_x, current_block_y = next_block
+        current_block_x, current_block_y = current_block_to_investigate
         self.current_coordinates = (current_block_x, current_block_y)
 
         if self.already_visited_block(block_coordinates=self.current_coordinates):
@@ -46,35 +46,35 @@ class Bfs(SearchAlgorithm):
             neighbour = (current_block_x + self.blocksize, current_block_y)
             if not self.already_visited_block(block_coordinates=neighbour):
                 self.change_block_color(block_coordinates=neighbour,
-                                        desired_block_color=BlockColors.blue.value)
+                                        desired_block_color_value=BlockColors.blue.value)
                 self.block_queue.put(neighbour)
 
         if self.is_valid_coordinate(block_coordinates=(current_block_x - self.blocksize, current_block_y)):
             neighbour = (current_block_x - self.blocksize, current_block_y)
             if not self.already_visited_block(block_coordinates=neighbour):
                 self.change_block_color(block_coordinates=neighbour,
-                                        desired_block_color=BlockColors.blue.value)
+                                        desired_block_color_value=BlockColors.blue.value)
                 self.block_queue.put(neighbour)
 
         if self.is_valid_coordinate(block_coordinates=(current_block_x, current_block_y - self.blocksize)):
             neighbour = (current_block_x, current_block_y - self.blocksize)
             if not self.already_visited_block(block_coordinates=neighbour):
                 self.change_block_color(block_coordinates=neighbour,
-                                        desired_block_color=BlockColors.blue.value)
+                                        desired_block_color_value=BlockColors.blue.value)
                 self.block_queue.put(neighbour)
 
         if self.is_valid_coordinate(block_coordinates=(current_block_x, current_block_y + self.blocksize)):
             neighbour = (current_block_x, current_block_y + self.blocksize)
             if not self.already_visited_block(block_coordinates=neighbour):
                 self.change_block_color(block_coordinates=neighbour,
-                                        desired_block_color=BlockColors.blue.value)
+                                        desired_block_color_value=BlockColors.blue.value)
                 self.block_queue.put(neighbour)
 
         self.visited_this_block(block_coordinates=self.current_coordinates)
 
         self.change_block_color(block_coordinates=self.current_coordinates,
-                                desired_block_color=BlockColors.lightblue.value)
+                                desired_block_color_value=BlockColors.lightblue.value)
         self.change_block_color(block_coordinates=self.destination_coordinates,
-                                desired_block_color=BlockColors.red.value)
+                                desired_block_color_value=BlockColors.red.value)
 
 
