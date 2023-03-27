b1b04b70a2f126b24a942243d27d5b6776c0918b
flizzyflo <vip3r1@gmx.de>
2023-03-27 18:45:05 -0400

    refactoring abstract class

--- src/search_algorithms/path_finding_algorithms/path_finding_algorithm_abstract.py
+++ src/search_algorithms/path_finding_algorithms/path_finding_algorithm_abstract.py
@@ -1,6 +1,7 @@
+from abc import ABC, abstractmethod
+
 from src.map_structure.map_structure import MapStructure
 from src.search_algorithms.search_algorithm_abstract_base_class import SearchAlgorithm
-from abc import ABC, abstractmethod
 
 
 class PathfindingAlgorithm(SearchAlgorithm, ABC):
@@ -8,5 +9,5 @@ class PathfindingAlgorithm(SearchAlgorithm, ABC):
         super().__init__(map_structure=map_structure)
 
     @abstractmethod
-    def backtrack_path(self, current_coordinates: tuple[int, int], node_list: list = list()) -> list[tuple[int, int]]:
+    def backtrack_path(self, current_coordinates: tuple[int, int]) -> list[tuple[int, int]]:
         pass
